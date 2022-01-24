c5e730b05c08d21348ca3a480ef4e47b84fffa57
Renan <renanthompsom@gmail.com>
2022-01-24 10:10:49 -0300

    First commit

---
+++ bundle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Bundle started"
+
+# Bundle docs into zero-dependency HTML file
+npx redoc-cli bundle api.yaml && \
+mv redoc-static.html index.html && \
+echo "Changed name from redoc-static.html to index.html" && \
+echo -e "\nDone!"
